1b87e671838bae2b2d9d7a170dc32c498b51fd64
Cees-Jan Kiewiet <ceesjank@gmail.com>
2014-08-17 18:10:42 +0200

    First prototype

---
+++ src/HelperTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace WyriHaximus\PHPUnit\Helpers;
+
+/**
+ * Class ReflectionTrait
+ * @package WyriHaximus\PHPUnit\Helpers
+ */
+class ReflectionTrait {
+    /**
+     * @param string $class
+     * @param string $name
+     * @return \ReflectionMethod
+     */
+    protected static function getMethod($class, $name) {
+        $class = new \ReflectionClass($class);
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+        return $method;
+    }
+
+}
